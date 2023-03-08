1e3ff08decda5dfaacc5aea73e3b407b8637f43f
“Imrannasu0506” <developerimru@gmail.com>
2023-03-08 11:00:43 +0000

    second commit

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
-  response.send('Hello DT!')
+  response.send('Hello Magic!')
 })
 
 app.listen(app.get('port'), function() {
